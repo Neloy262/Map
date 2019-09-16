e203d02b85bbe3c018b08ef103b36bfb467c37f8
Neloy262 <neloysarwar@gmail.com>
2019-09-16 14:33:34 +0600

    Initial commit

---
+++ app/src/main/java/com/example/map_v/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.map_v;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(23.8376, 90.3581);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Bangladesh"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
